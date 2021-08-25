50ed0c47ace5e58e34f1f8041854f05dca60ee9b
Tobias Høegh <tobias@tujo.no>
2021-08-25 20:33:11 +0200

    chore: add missing mock files

---
+++ integration-tests/__mocks__/.cache/wpe-scopes.js
@@ -0,0 +1,3 @@
+export * as _26243a013ca7d319d87081f9768dd680 from '../src/pages/first-scope/second-scope/wrap-pages.js'
+export * as _455a0f696332624bb2ff3f910434400d from '../src/pages/first-scope/wrap-pages.js'
+export * as _de24c938e6d0ae34eea46b0360bc707c from '../src/pages/wrap-pages.js'
