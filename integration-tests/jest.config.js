6ce1931ee737099f360576a951c62c76f2bccbaa
Tobias Høegh <tobias@tujo.no>
2021-09-10 12:20:32 +0200

    chore: ensure jest watches the main module as well

--- integration-tests/jest.config.js
+++ integration-tests/jest.config.js
@@ -2,8 +2,9 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 
 module.exports = {
-  verbose: true,
+  verbose: true, // each individual test should be reported during the run
   testEnvironment: 'jsdom',
+  roots: ['<rootDir>/__tests__/', '<rootDir>../gatsby-plugin-wrap-pages/'], // enables file watcher for gatsby-plugin-wrap-pages
   transform: { '\\.[jt]sx?$': 'babel-jest' },
   globals: {
     WDE_CACHE_PATH: require.resolve('./__mocks__/.cache/wpe-scopes.js'),
