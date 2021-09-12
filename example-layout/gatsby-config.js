dcfb5ce37b944ba76dbea4378d73dc6a5f07cdac
Tobias Høegh <tobias@tujo.no>
2021-09-12 20:21:03 +0200

    chore: remove layout example

--- example-layout/gatsby-config.js
+++
@@ -1,6 +0,0 @@
-exports.plugins = [
-  {
-    resolve: 'gatsby-plugin-wrap-pages',
-    options: { wrapperName: 'layout.tsx' },
-  },
-]
--- example-layout/src/pages/404.tsx
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import React from 'react'
-// @ts-ignore
-import { PageProps } from 'gatsby'
-
-export default function NotFound(props: PageProps) {
-  return <>404</>
-}
